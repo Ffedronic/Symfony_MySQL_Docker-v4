e0eff8c2334b96b0678b0a9c3ae8428c1e56dacc
Your Name <you@example.com>
2023-08-05 22:37:13 +0200

    reservation form

--- project/src/Controller/FrontController.php
+++ project/src/Controller/FrontController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Customer;
+use App\Entity\Reservation;
+use App\Form\CustomerFormType;
+use App\Form\ReservationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +15,12 @@ class FrontController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $reservation = new Reservation;
+        $reservationForm = $this -> createForm(ReservationFormType::class, $reservation);
+
         return $this->render('front/index.html.twig', [
             'controller_name' => 'FrontController',
+            'reservation_form'=> $reservationForm->createView(),
         ]);
     }
 }
